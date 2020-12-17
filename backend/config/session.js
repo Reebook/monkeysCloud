ee1c0889fc03ea073c201cc514270d17310fa0f1
Carlos Ezeta A <ezeta506@gmail.com>
2020-12-16 19:46:17 -0600

    config

--- backend/config/session.js
+++ backend/config/session.js
@@ -23,7 +23,7 @@ module.exports.session = {
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '1234',
+  password: '123456',
   database: 'monkeycloud',
   ssl: false,
   stringify: true,
