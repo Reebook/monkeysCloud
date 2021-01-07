371bdc19d339926691c85596f9ab0e3c92050abb
oscar98colibricode <oscar.alvarez@colibricode.com>
2021-01-07 16:15:38 -0600

    actualizaciones

--- frontend/src/components/newTask/index.js
+++ frontend/src/components/newTask/index.js
@@ -1,81 +1,19 @@
 import React, {useState, useMemo, useEffect} from 'react';
-import {useDropzone} from 'react-dropzone';
 import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import axios from 'axios';
 
 //Material UI
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+//antd-img-crop
+/* import ImgCrop from 'antd-img-crop';
+import { Upload } from 'antd'; */
 
 import './style.scss';
 
-
-const thumbsContainer = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 16
-};
-
-const thumb = {
-    display: 'inline-flex',
-    borderRadius: 2,
-    border: '1px solid #eaeaea',
-    marginBottom: 8,
-    marginRight: 8,
-    width: 100,
-    height: 100,
-    padding: 4,
-    boxSizing: 'border-box'
-};
-
-const thumbInner = {
-    display: 'flex',
-    minWidth: 0,
-    overflow: 'hidden'
-};
-
-const img = {
-    display: 'block',
-    width: 'auto',
-    height: '100%'
-};
-
-const baseStyle = {
-  flex: 1,
-  display: 'flex',
-  flexDirection: 'column',  
-  padding: '20px',
-  borderWidth: 2,
-  borderRadius: 2,
-  borderColor: '#aeafb1',
-  borderStyle: 'dashed',
-  backgroundColor: '#fafafa',
-  color: '#bdbdbd',
-  outline: 'none',
-  transition: 'border .24s ease-in-out',
-  fontWeight: '300',
-  textAlign: 'center',
-  fontSize: '14px',
-  lineHeight: '20px'
-};
-
-const activeStyle = {
-  borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-  borderColor: '#00e676'
-};
-
-const rejectStyle = {
-  borderColor: '#ff1744'
-};
-
 const NewTask = (props) =>{
     const [projects, setProjects] = useState([]);
-    const [components, setComponents] = useState([]);        
-    const [files, setFiles] = useState([]);    
+    const [components, setComponents] = useState([]);            
 
     const [form, setForm] = useState({
         relatedProject: '',
@@ -87,44 +25,6 @@ const NewTask = (props) =>{
         description: ''
     });
     
-    const {
-        getRootProps,
-        getInputProps,
-        isDragActive,
-        isDragAccept,
-        isDragReject
-    } = useDropzone({        
-        onDrop: acceptedFiles => {
-        setFiles(acceptedFiles.map(file => Object.assign(file, {            
-            preview: URL.createObjectURL(file)
-        })));
-        }
-    });
-
-    const style = useMemo(() => ({
-        ...baseStyle,
-        ...(isDragActive ? activeStyle : {}),
-        ...(isDragAccept ? acceptStyle : {}),
-        ...(isDragReject ? rejectStyle : {})
-    }), [isDragActive, isDragReject, isDragAccept]);
-
-    const thumbs = files.map(file => (
-        <div style={thumb} key={file.name}>
-            <div style={thumbInner}>
-                <img
-                src={file.preview}
-                style={img}
-                />
-            </div>
-        </div>
-    ));
-
-    useEffect(() => () => {
-        // Make sure to revoke the data uris to avoid memory leaks
-        files.forEach(file => URL.revokeObjectURL(file.preview));
-    }, [files]);
-
-
     useEffect(() => {
         getProjects();
         getComponents();
@@ -205,9 +105,9 @@ const NewTask = (props) =>{
                                 id="taskPriority"
                                 name="priority"
                                 value={form.priority}
-                                onChange={handleFormChange}
+                                onChange={handleFormChange}                                
                             >                                                    
-                                <MenuItem value={"Highest"}><FaArrowUp style={{color:'#e60000' }} className="priority-style"/>Highest</MenuItem>
+                                <MenuItem value={"Highest"}><FaArrowUp style={{color:'#e60000'}} className="priority-style"/>Highest</MenuItem>
                                 <MenuItem value={"High"}><FaArrowUp style={{color:'#ff6666'}} className="priority-style"/>High</MenuItem>
                                 <MenuItem value={"Medium"}><FaArrowUp style={{color:'#ff751a'}} className="priority-style"/>Medium</MenuItem>
                                 <MenuItem value={"Low"}><FaArrowDown style={{color:'#008000'}} className="priority-style"/>Low</MenuItem>
@@ -215,9 +115,15 @@ const NewTask = (props) =>{
                             </Select> 
                         </div>                    
                         <p className="description">Some issue types are unavailable due to incompatible field configuration and/or workflow associations.</p>
-                        <hr/>                    
+                        <hr/> 
+                        <div className="input-divider">
+                            <label className="title-control">Summary</label>                                                        
+                        </div>                   
                         <div className="input-divider">                        
-                            <input type="text" className="form-control" placeholder="Summary" value={form.summary} name="summary" onChange={handleFormChange}/>
+                            <input type="text" className="form-control" autoComplete="off" value={form.summary} name="summary" onChange={handleFormChange}/>
+                        </div>
+                        <div className="input-divider">
+                            <label className="title-control">Component</label>                                                        
                         </div>
                         <div className="input-divider">                                                                            
                             <Select className="form-control"                            
@@ -232,16 +138,18 @@ const NewTask = (props) =>{
                                     ))
                                 }                                                                                                                                  
                             </Select> 
-                        </div>                    
-                        <section className="upload-container">
-                            <div {...getRootProps({style})}>
-                                <input {...getInputProps()} name="attachment" value={form.attachment} onChange={handleFormChange} />                                
-                                <p style={{color:"#aeafb1"}}>Drop files here to attach, or <a href="#" style={{color:"#0759bd"}}>browse</a></p>
-                                <aside style={thumbsContainer}>
-                                    {thumbs}
-                                </aside>
-                            </div>                            
-                        </section>                        
+                        </div>         
+                        <div className="input-divider">
+                            <label className="title-control">Attachment</label>                                                        
+                        </div>           
+
+                        <section className="input-divider">
+            
+                        </section>
+
+                        <div className="input-divider">
+                            <label className="title-control">Description</label>                                                        
+                        </div>                   
                         <div className="input-divider">
                             <textarea name="description" className="form-control" rows="10" value={form.description} onChange={handleFormChange}></textarea>
                         </div>
